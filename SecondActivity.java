3a549f4cc1ae924b9a5b1ae36d031e61f4e1dfac
fizadalvi <140835031+fizadalvi@users.noreply.github.com>
2024-04-06 09:46:33 +0530

    Add files via upload

---
+++ SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.login;
+
+
+package com.example.android.logindemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
+
